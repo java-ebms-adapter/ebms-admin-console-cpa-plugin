471347f632a3254bcb4f0d8d57520b4c05615599
eluinstra <e.luinstra@gmail.com>
2016-04-23 17:11:44 +0200

    implementing generate cpa

--- src/main/java/nl/clockwork/ebms/admin/plugin/cpa/common/Utils.java
+++ src/main/java/nl/clockwork/ebms/admin/plugin/cpa/common/Utils.java
@@ -5,24 +5,32 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.security.GeneralSecurityException;
-import java.security.KeyStore;
+import java.security.KeyException;
 import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Enumeration;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.TimeZone;
 
 import javax.xml.crypto.MarshalException;
 import javax.xml.crypto.dom.DOMStructure;
 import javax.xml.crypto.dsig.XMLSignatureFactory;
 import javax.xml.crypto.dsig.keyinfo.KeyInfo;
 import javax.xml.crypto.dsig.keyinfo.KeyInfoFactory;
+import javax.xml.namespace.NamespaceContext;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
 
 import nl.clockwork.ebms.common.util.DOMUtils;
 
@@ -31,19 +39,60 @@ import org.w3c.dom.Node;
 
 public class Utils
 {
-	public static X509Certificate getCertificateBySubject(KeyStore keyStore, String subject, String password) throws GeneralSecurityException
+	public static XPath createXPath()
 	{
-		Enumeration<String> aliases = keyStore.aliases();
-		while (aliases.hasMoreElements())
+		XPath result = XPathFactory.newInstance().newXPath();
+		result.setNamespaceContext(new NamespaceContext()
 		{
-			String alias = aliases.nextElement();
-			X509Certificate certificate = (X509Certificate)keyStore.getCertificate(alias);
-			if (certificate.getSubjectDN().getName().equals(subject))
-				return certificate;
-		}
-		return null;
+	    public String getNamespaceURI( String prefix)
+	    {
+	      if (prefix.equals("cpa"))
+	        return "http://www.oasis-open.org/committees/ebxml-cppa/schema/cpp-cpa-2_0.xsd";
+	      else if(prefix.equals("xmldsig"))
+	      	return "http://www.w3.org/2000/09/xmldsig#";
+	      return "";//XPathConstants.NULL_NS_URI;
+	    }
+
+			@Override
+			public String getPrefix(String namespaceURI)
+			{
+	      if (namespaceURI.equals("http://www.oasis-open.org/committees/ebxml-cppa/schema/cpp-cpa-2_0.xsd"))
+	        return "cpa";
+	      else if (namespaceURI.equals("http://www.w3.org/2000/09/xmldsig#"))
+	        return "xmldsig";
+	      return null;
+			}
+
+			@Override
+			public Iterator<String> getPrefixes(String namespaceURI)
+			{
+				ArrayList<String> result = new ArrayList<String>();
+	      if (namespaceURI.equals("http://www.oasis-open.org/committees/ebxml-cppa/schema/cpp-cpa-2_0.xsd"))
+	        result.add("cpa");
+	      else if (namespaceURI.equals("http://www.w3.org/2000/09/xmldsig#"))
+	      	result.add("xmldsig");
+				return result.iterator();
+			}
+	  });
+		return result;
 	}
 
+	public static String toXSDDate(Date date)
+	{
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'00:00:00X");
+		df.setTimeZone(TimeZone.getTimeZone("GMT"));
+		return df.format(date);
+	}
+
+	public static void generateKeyInfo(Node node, InputStream is) throws CertificateException, KeyException, MarshalException
+	{
+    KeyInfoFactory kif = XMLSignatureFactory.getInstance("DOM").getKeyInfoFactory();
+    Certificate certificate = CertificateFactory.getInstance("X.509").generateCertificate(is);
+    KeyInfo keyInfo = kif.newKeyInfo(Arrays.asList(kif.newKeyValue(certificate.getPublicKey()),kif.newX509Data(Collections.singletonList(certificate))));
+    DOMStructure dom = new DOMStructure(node);
+    keyInfo.marshal(dom,null);
+	}
+	
 	public static void write(Node node, OutputStream outputStream) throws TransformerException
 	{
 		Transformer transformer = DOMUtils.getTransformer();
--- src/main/java/nl/clockwork/ebms/admin/plugin/cpa/web/CreateCPAPage.java
+++ src/main/java/nl/clockwork/ebms/admin/plugin/cpa/web/CreateCPAPage.java
@@ -15,22 +15,18 @@
  */
 package nl.clockwork.ebms.admin.plugin.cpa.web;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.security.KeyException;
+import java.security.cert.CertificateException;
 import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Iterator;
 import java.util.List;
-import java.util.TimeZone;
 
-import javax.xml.bind.DatatypeConverter;
-import javax.xml.namespace.NamespaceContext;
+import javax.xml.crypto.MarshalException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
 
+import nl.clockwork.ebms.admin.plugin.cpa.common.Utils;
 import nl.clockwork.ebms.admin.plugin.cpa.dao.CPAPluginDAO;
 import nl.clockwork.ebms.admin.plugin.cpa.model.CPATemplate;
 import nl.clockwork.ebms.admin.web.BasePage;
@@ -50,6 +46,8 @@ import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.ChoiceRenderer;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.upload.FileUpload;
+import org.apache.wicket.markup.html.form.upload.FileUploadField;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
@@ -57,6 +55,7 @@ import org.apache.wicket.model.Model;
 import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.apache.wicket.util.io.IClusterable;
+import org.springframework.util.StringUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -99,6 +98,7 @@ public class CreateCPAPage extends BasePage
 		public CreateCPAForm(String id)
 		{
 			super(id,new CompoundPropertyModel<CreateCPAFormModel>(new CreateCPAFormModel()));
+			setMultiPart(true);
 			add(new BootstrapFormComponentFeedbackBorder("cpaTemplateFeedback",createCPATemplateChoice("cpaTemplate")));
 			add(new BootstrapFormComponentFeedbackBorder("cpaIdFeedback",new TextField<String>("cpaId",new LocalizedStringResource("lbl.cpaId",CreateCPAForm.this)).setRequired(true)));
 			add(new BootstrapFormComponentFeedbackBorder("startDateFeedback",new BootstrapDateTimePicker("startDate",new LocalizedStringResource("lbl.startDate",CreateCPAForm.this),"dd-MM-yyyy",BootstrapDateTimePicker.Type.DATE).setRequired(true)));
@@ -106,6 +106,7 @@ public class CreateCPAPage extends BasePage
 			add(new BootstrapFormComponentFeedbackBorder("partyNameFeedback",new TextField<String>("partyName",new LocalizedStringResource("lbl.partyName",CreateCPAForm.this)).setRequired(true)));
 			add(new BootstrapFormComponentFeedbackBorder("partyIdFeedback",new TextField<String>("partyId",new LocalizedStringResource("lbl.partyId",CreateCPAForm.this)).setRequired(true)));
 			add(new BootstrapFormComponentFeedbackBorder("urlFeedback",new TextField<String>("url",new LocalizedStringResource("lbl.url",CreateCPAForm.this)).setRequired(true)));
+			add(new BootstrapFormComponentFeedbackBorder("clientCertificateFileFeedback",createCertificateFileField("clientCertificateFile","lbl.clientCertificateFile")));
 			//ClientCertificate
 			//ServerCertificate
 			//SigningCertificate
@@ -130,6 +131,22 @@ public class CreateCPAPage extends BasePage
 			return result;
 		}
 
+		private FileUploadField createCertificateFileField(String id, final String label)
+		{
+			FileUploadField result = new FileUploadField(id)
+			{
+				private static final long serialVersionUID = 1L;
+
+				@Override
+				public IModel<String> getLabel()
+				{
+					return Model.of(getLocalizer().getString(label,CreateCPAForm.this));
+				}
+			};
+			result.setRequired(true);
+			return result;
+		}
+
 		private Button createGenerateButton(String id)
 		{
 			Button result = new Button(id,new ResourceModel("cmd.generate"))
@@ -144,25 +161,26 @@ public class CreateCPAPage extends BasePage
 						CPATemplate cpaTemplate = CreateCPAForm.this.getModelObject().cpaTemplate;
 						Document document = DOMUtils.read(cpaTemplate.getContent());
 						processDocument(document,CreateCPAForm.this.getModelObject());
-						cpaService.insertCPA(DOMUtils.toString(document),null,false);
-						setResponsePage(new CPAsPage());
+						System.out.println(DOMUtils.toString(document));
+//						cpaService.insertCPA(DOMUtils.toString(document),null,false);
+//						setResponsePage(new CPAsPage());
 					}
 					catch (Exception e)
 					{
 						logger.error("",e);
-						error(e.getMessage());
+						error(StringUtils.isEmpty(e.getMessage()) ? e : e.getMessage());
 					}
 				}
 
-				private void processDocument(Document document, CreateCPAFormModel modelObject) throws XPathExpressionException
+				private void processDocument(Document document, CreateCPAFormModel modelObject) throws XPathExpressionException, CertificateException, KeyException, MarshalException, IOException
 				{
-					XPath xpath = createXPath();
+					XPath xpath = Utils.createXPath();
 					Node node = (Node)xpath.evaluate("/cpa:CollaborationProtocolAgreement/@cpa:cpaid",document,XPathConstants.NODE);
 					node.setNodeValue(modelObject.cpaId);
 					node = (Node)xpath.evaluate("/cpa:CollaborationProtocolAgreement/cpa:Start/text()",document,XPathConstants.NODE);
-					node.setNodeValue(toXSDDate(modelObject.startDate));
+					node.setNodeValue(Utils.toXSDDate(modelObject.startDate));
 					node = (Node)xpath.evaluate("/cpa:CollaborationProtocolAgreement/cpa:End/text()",document,XPathConstants.NODE);
-					node.setNodeValue(toXSDDate(modelObject.endDate));
+					node.setNodeValue(Utils.toXSDDate(modelObject.endDate));
 					node = (Node)xpath.evaluate("/cpa:CollaborationProtocolAgreement/cpa:PartyInfo[2]/@cpa:partyName",document,XPathConstants.NODE);
 					node.setNodeValue(modelObject.partyName);
 					node = (Node)xpath.evaluate("/cpa:CollaborationProtocolAgreement/cpa:PartyInfo[2]/cpa:PartyId/text()",document,XPathConstants.NODE);
@@ -170,50 +188,23 @@ public class CreateCPAPage extends BasePage
 					NodeList nodeList = (NodeList)xpath.evaluate("/cpa:CollaborationProtocolAgreement/cpa:PartyInfo[2]/cpa:Transport/cpa:TransportReceiver/cpa:Endpoint/@cpa:uri",document,XPathConstants.NODESET);
 					for (int i = 0; i < nodeList.getLength(); i++)
 						nodeList.item(i).setNodeValue(modelObject.url);
+					final List<FileUpload> files = modelObject.clientCertificateFile;
+					if (files != null && files.size() == 1)
+					{
+						node = (Node)xpath.evaluate("/cpa:CollaborationProtocolAgreement/cpa:PartyInfo[2]/cpa:Certificate[1]",document,XPathConstants.NODE);
+						node = (Node)xpath.evaluate("//cpa:Certificate[@cpa:certId = 'OVERHEID_SigningCert']",document,XPathConstants.NODE);
+						//node = (Node)xpath.evaluate("//cpa:Certificate[@cpa:certId = /cpa:CollaborationProtocolAgreement/@cpa:cpaid]",document,XPathConstants.NODE);
+						//node = (Node)xpath.evaluate("id(/cpa:CollaborationProtocolAgreement/cpa:PartyInfo[2]//cpa:SigningCertificateRef/@cpa:certId)",document,XPathConstants.NODE);
+						Node node1 = (Node)xpath.evaluate("/cpa:CollaborationProtocolAgreement/cpa:PartyInfo[2]/cpa:Certificate[1]/xmldsig:KeyInfo",document,XPathConstants.NODE);
+						node.removeChild(node1);
+						Utils.generateKeyInfo(node,files.get(0).getInputStream());
+					}
 				}
 
-				private String toXSDDate(Date date)
-				{
-					DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'00:00:00X");
-					df.setTimeZone(TimeZone.getTimeZone("GMT"));
-					return df.format(date);
-				}
 			};
 			setDefaultButton(result);
 			return result;
 		}
-	}
-
-	public static XPath createXPath()
-	{
-		XPath result = XPathFactory.newInstance().newXPath();
-		result.setNamespaceContext(new NamespaceContext()
-		{
-	    public String getNamespaceURI( String prefix)
-	    {
-	      if (prefix.equals("cpa"))
-	        return "http://www.oasis-open.org/committees/ebxml-cppa/schema/cpp-cpa-2_0.xsd";
-	      return "";//XPathConstants.NULL_NS_URI;
-	    }
-
-			@Override
-			public String getPrefix(String namespaceURI)
-			{
-	      if (namespaceURI.equals("http://www.oasis-open.org/committees/ebxml-cppa/schema/cpp-cpa-2_0.xsd"))
-	        return "cpa";
-	      return null;
-			}
-
-			@Override
-			public Iterator<String> getPrefixes(String namespaceURI)
-			{
-				ArrayList<String> result = new ArrayList<String>();
-	      if (namespaceURI.equals("http://www.oasis-open.org/committees/ebxml-cppa/schema/cpp-cpa-2_0.xsd"))
-	        result.add("cpa");
-				return result.iterator();
-			}
-	  });
-		return result;
 	}
 
 	public class CreateCPAFormModel implements IClusterable
@@ -226,6 +217,7 @@ public class CreateCPAPage extends BasePage
 		private String partyName;
 		private String partyId;
 		private String url;
+		private List<FileUpload> clientCertificateFile;
 
 		public CPATemplate getCpaTemplate()
 		{
@@ -282,6 +274,14 @@ public class CreateCPAPage extends BasePage
 		public void setUrl(String url)
 		{
 			this.url = url;
+		}
+		public List<FileUpload> getClientCertificateFile()
+		{
+			return clientCertificateFile;
+		}
+		public void setClientCertificateFile(List<FileUpload> clientCertificateFile)
+		{
+			this.clientCertificateFile = clientCertificateFile;
 		}
 	}
 }
